a24b28c84e3d964319ed2d31c115e4551984e3a6
tuyizerejean <tuyibobu@gmail.com>
2022-03-30 18:49:27 +0200

    Displaying data to document

--- todo.js
+++ todo.js
@@ -2,12 +2,30 @@
 function createTodos() {
     const todoForm=document.getElementById("formDiv")
     todoForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-      const todoNme=document.getElementById("name").value
-      const todoDate=document.getElementById("todDate").value
-      console.log(todoNme,todoDate )
-      event.target.elements.name.value = "";
-      event.target.elements.todDate.value = "";  
+    event.preventDefault();
+    const todoNme=document.getElementById("name").value
+    const todoDate=document.getElementById("todDate").value
+    event.target.elements.name.value = "";
+    event.target.elements.todDate.value = "";  
+    const viewTodo=document.getElementById("todoView")
+    const divTodo=document.createElement("div")
+    divTodo.setAttribute("class","formDiv")
+    divTodo.setAttribute("class","pb-3 pl-5")
+    viewTodo.appendChild(divTodo)
+    const listTodo=document.createElement("ul")
+    divTodo.appendChild(listTodo)
+    const doneTodo=document.createElement("div")
+    doneTodo.setAttribute("class","bg-green-500")
+    divTodo.appendChild(doneTodo)
+    const tickDoneTodo=document.createElement("input")
+    tickDoneTodo.setAttribute("type","checkbox")
+    divTodo.appendChild(tickDoneTodo)
+    const nameTodo=document.createElement("label")
+    nameTodo.innerHTML=todoNme
+    divTodo.appendChild(nameTodo)
+    const dateTodos=document.createElement("p")
+    dateTodos.innerHTML=todoDate
+    divTodo.appendChild(dateTodos)
     });
 }
 createTodos()
