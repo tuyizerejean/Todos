021fbddefeac6d019fac21236d8a1ac677994b47
tuyizerejean <tuyibobu@gmail.com>
2022-03-31 23:44:57 +0200

    validating the form

--- todo.js
+++ todo.js
@@ -7,27 +7,35 @@
 
     event.target.elements.name.value = "";
     event.target.elements.todDate.value = ""; 
-    const viewTodo=document.getElementById("todoView")
-    const divTodo=document.createElement("div")
-    divTodo.setAttribute("class","formDiv")
-    const listTodo=document.createElement("ul")
-    divTodo.setAttribute("class","incompleted-tasks")
-   const list=document.createElement("li")
-    const doneTodo=document.createElement("div")
-    doneTodo.setAttribute('class','bg-blue-500 p-3')
-    const tickDoneTodo=document.createElement("input")
-    tickDoneTodo.setAttribute("type","checkbox")
-    const nameTodo=document.createElement("label")
-    nameTodo.innerHTML=todoNme
-    const dateTodos=document.createElement("p")
-    dateTodos.innerHTML=`<i>${todoDate}</i>`
-    doneTodo.appendChild(tickDoneTodo)
-    doneTodo.appendChild(nameTodo)
-    list.appendChild(dateTodos)
-    list.appendChild(doneTodo)
-    listTodo.appendChild(list)
-    divTodo.appendChild(listTodo)
-    viewTodo.appendChild(divTodo)
+    if(todoNme && todoDate )
+    {
+        const viewTodo=document.getElementById("todoView")
+        const divTodo=document.createElement("div")
+        divTodo.setAttribute("class","formDiv")
+        const listTodo=document.createElement("ul")
+        divTodo.setAttribute("class","incompleted-tasks")
+       const list=document.createElement("li")
+        const doneTodo=document.createElement("div")
+        doneTodo.setAttribute('class','bg-blue-500 p-3')
+        const tickDoneTodo=document.createElement("input")
+        tickDoneTodo.setAttribute("type","checkbox")
+        const nameTodo=document.createElement("label")
+        nameTodo.innerHTML=todoNme
+        const dateTodos=document.createElement("p")
+        dateTodos.innerHTML=`<i>${todoDate}</i>`
+        doneTodo.appendChild(tickDoneTodo)
+        doneTodo.appendChild(nameTodo)
+        list.appendChild(dateTodos)
+        list.appendChild(doneTodo)
+        listTodo.appendChild(list)
+        divTodo.appendChild(listTodo)
+        viewTodo.appendChild(divTodo)
+    }
+    else{
+        document.querySelector("#validatError").style.display = "block" 
+        
+    }
+ 
     });
 
 const checkbox=document.getElementById("cheked")
